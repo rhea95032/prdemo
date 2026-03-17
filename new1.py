9b11bb5b66ed533cd826b0a97d4175db3cb2751d
Eric <rhea95032@gmail.com>
2026-03-17 15:01:30 +0800

    1.1

--- new1.py
+++ new1.py
@@ -6,5 +6,10 @@ password="!Rhea95032"
 database="free-sql-db-7651785"
 
 connect=pymssql.connect(server,user,password,database)
-
 print ("登入成功")
+
+cursor=connect.cursor()
+print("cursor成功")
+
+cursor.execute("select * from stocks")
+print("查詢成功")
